15e7ef9d96cdec0923898fcca9654293c251e718
Mahmoud Elshenawy <mahmoudelshenawy@aucegypt.edu>
2023-11-21 03:21:41 +0300

    added the pacman block memory along with its test bench

--- backend/sim/pacman_map_blockmem_tb.v
+++ backend/sim/pacman_map_blockmem_tb.v
@@ -2,17 +2,14 @@
 module pacman_map_blockmem_tb();
 
     reg clka;
-    reg ena;
-    reg wea;
+    
     reg [5:0] addra;
-    reg [127:0] dina; 
-    wire [127:0] douta; 
+    wire [79:0] douta; 
     
     
    
         
    pacman_map_blockmem uut_pacman_map_blockmem (.clka(clka),
-                                         .ena(ena),
                                          
                                          .addra(addra),
                                          
@@ -21,11 +18,9 @@ module pacman_map_blockmem_tb();
 
     initial begin
     
-        // initial values for the enables 
-        ena= 0; // this is to read 
-        wea=0; // this is to disable writing 
-        dina=-128'd1; // this will not be written 
-        clka=0; 
+        // initial values for the clock  
+        
+        clka=1; 
         
        
        
@@ -33,10 +28,10 @@ module pacman_map_blockmem_tb();
         addra= $unsigned(6'd0); // read the first row with 80 values [128 bit padded with zeros]
         
         #10
-       addra= $unsigned(6'd63); // read the last row 
+       addra= $unsigned(6'd3); // read the fourth row
         
 
-        #10 $finish;
+        #100 $finish;
 
     end
     
--- backend/src/pacman_map_blockmem.v
+++ backend/src/pacman_map_blockmem.v
@@ -1,22 +1,23 @@
 
 
 module pacman_map_blockmem (input clka,
-                            input ena,
                             
-                           input [5:0] addra, 
                            
-                           output [127:0] douta 
+                           input [5:0] addra, 
+                             
+                           output [79:0] douta 
                             
                             ); 
 
 
-
+    wire ena=1; 
     pacman_map_blk_mem_gen pacman_map_inst(.clka(clka),
                                          .ena(ena),
-                                       
+                                         
                                          .addra(addra),
                                         
                                          .douta(douta)); 
+
 
 
 
