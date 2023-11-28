b6c5ad4cb02753226676b01b013d431d72714b4d
Mahmoud Elshenawy <mahmoudelshenawy@aucegypt.edu>
2023-11-28 19:00:30 +0300

    tried a new compact way of coding the bcd converter but it did not work out

--- backend/sim/bcd_converter_tb.v
+++ backend/sim/bcd_converter_tb.v
@@ -1,8 +1,8 @@
 module bcd_converter_tb(); 
 
 
-    parameter INPUT_WIDTH= 5;
-    parameter DECIMAL_DIGITS= 2; 
+    parameter INPUT_WIDTH= 11;
+    parameter DECIMAL_DIGITS= 4; 
 
     reg                         clka;
     reg [INPUT_WIDTH-1:0]       i_Binary;
@@ -13,23 +13,26 @@ module bcd_converter_tb();
 
    
 
-    bcd_converter
-      #(.INPUT_WIDTH (5),
-        .DECIMAL_DIGITS(2)) uut_bcd_converter
-      (
-       .i_Clock(clka),
-       .i_Binary(i_Binary),
-       .i_Start(i_Start),
-       //
-       .o_BCD(o_BCD),
-       .o_DV (o_DV)
-       );
+//    bcd_converter
+//      #(.INPUT_WIDTH (INPUT_WIDTH),
+//        .DECIMAL_DIGITS(DECIMAL_DIGITS)) uut_bcd_converter
+//      (
+//       .i_Clock(clka),
+//       .i_Binary(i_Binary),
+//       .i_Start(i_Start),
+//       //
+//       .o_BCD(o_BCD),
+//       .o_DV (o_DV)
+//       );
 
+    bcd_converter uut_bcd_converter (
+            .bin(i_Binary),
+             .bcd(o_BCD));
     initial begin
         clka = 1 ; 
         i_Start = 1; 
          
-        i_Binary = 5'd23; 
+        i_Binary = 11'd1024; 
         
          
         
@@ -41,7 +44,7 @@ module bcd_converter_tb();
        
        
        
-        #1000 $finish;
+        #10000 $finish;
 
     end
     
